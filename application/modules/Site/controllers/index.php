3c4976ff067904a2fe318312bb4821a19ad9a523
Alex <alex@simplecoding.net>
2012-11-12 19:49:12 +0200

    updates

--- application/modules/Site/controllers/index.php
+++ application/modules/Site/controllers/index.php
@@ -19,7 +19,7 @@
  */
 
 class Site_Controllers_Index extends SC_Controller{
-	function indexTrigger(){
-		$this->loadView('default');
-	}
+    function indexTrigger(){
+        $this->loadView('default');
+    }
 }
